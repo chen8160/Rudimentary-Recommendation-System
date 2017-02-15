1eeebbd3d743cd555cfb97f4b2d1931de83441ee
Yuxiang Chen <yuxiangc@iastate.edu>
2017-02-15 14:38:40 -0600

    Make up the skeleton of  "HashFunction" and "HashTable" classes

--- src/HashFunction.java
+++ src/HashFunction.java
@@ -1,4 +1,39 @@
 
 public class HashFunction {
 
+    private int a, b, p;
+
+    public HashFunction(int range){
+        //TODO: Constructor;
+    }
+
+    public int hash(int x){
+        //TODO: Return the hash value of x
+        return 0;
+    }
+
+    public int getA(){
+        return a;
+    }
+
+    public int getB(){
+        return b;
+    }
+
+    public int getP(){
+        return p;
+    }
+
+    public void setA(int x){
+        //TODO: update a
+    }
+
+    public void setB(int y){
+        //TODO: update b
+    }
+
+    public void setP(int x){
+        //TODO: update p
+    }
+
 }
--- src/HashTable.java
+++ src/HashTable.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
 
 public class HashTable {
+
+    ArrayList<Tuple> hashTable;
+
+    public HashTable(int size){
+        //TODO: Constructor
+    }
+
+    public int maxLoad(){
+        //TODO: return the max load of the hash table
+        return 0;
+    }
+
+    public int averageLoad(){
+        //TODO: return the avg load of the hash table
+        return 0;
+    }
+
+    public int size(){
+        //TODO: return the current size of the hash table
+        return 0;
+    }
+
+    public int numElements(){
+        //TODO: return the number of elements stored in the hash table
+        return 0;
+    }
+
+    public int loadFactor(){
+        //TODO: return the load factor
+        return 0;
+    }
+
+    public void add(Tuple t){
+        //TODO: add the tuple t to the hash table. Resize if necessary
+    }
+
+    public ArrayList<Tuple> search(int k){
+        //TODO: return an arraylist of tuples, which key==k
+        return null;
+    }
+
+    public void remove(Tuple t){
+        //TODO: remove tuple t from the hash table
+    }
 
 }
