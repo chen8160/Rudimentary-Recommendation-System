ad0ed91d46b9d3de2928497d52306d6203b8560f
chen8160 <chenyuxiang8160@gmail.com>
2017-02-21 00:12:14 -0600

    Template for Nearestpoints

--- src/NearestPoints.java
+++ src/NearestPoints.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+
 /**
  * @author Yuxiang Chem, YangTian
  */
 public class NearestPoints {
 
+
+    public NearestPoints(String dataFile) {
+    }
+
+    public NearestPoints(ArrayList<Float> pointSet) {
+    }
+
+    public ArrayList<Float> naiveNearestPoints(float p){
+        return null;
+    }
+
+    public void buildDataStructure(){
+
+    }
+
+    public ArrayList<Float> npHashNearestPoints(float p){
+        return null;
+    }
+
+    public void allNearestPointsNaive(){
+
+    }
+
+    public void allNearestPointsHash(){
+
+    }
 }
